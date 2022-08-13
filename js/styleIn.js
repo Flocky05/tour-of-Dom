63355b2cb269c51dfea4e431b95b025b43f6dac9
Flocky05 <md.rakibul.islam.0501@gmail.com>
2022-08-13 22:40:17 +0000

    added all files

---
+++ js/styleIn.js
@@ -0,0 +1,9 @@
+const sections=document.querySelectorAll('section');
+for(const section of sections){
+    section.style.border =" 2px solid blue";
+    section.style.marginTop="5px";
+    section.style.marginBottom ="5px";
+    section.style.borderRadius="5px";
+    section.style.paddingLeft="10px";
+    section.style.backgroundColor ="lightgray";
+}
